c4f410b8da037ab457b7a3a326312360e1deb39b
Elio Struyf <elio@struyfconsulting.be>
2021-12-12 16:53:20 +0100

    Restructuring webview

---
+++ solutions/webview/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className={``}>
+      <Head>
+        <title>Pages overview</title>
+        <meta name="description" content="Pages overview" />
+      </Head>
+
+      <main className={``}>
+        Main page
+      </main>
+    </div>
+  )
+}
+
+export default Home
